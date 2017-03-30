7366309650132279c63e21efbb820f690487876c
Yahya Gilany <yahya.gilany@live.com>
2017-03-29 21:07:30 -0400

    fire 2 terminals

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,30 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getConfig } from './config';
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ygilany-test" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+   
+    let disposable = vscode.commands.registerCommand('extension.FireDev', () => {
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let configurations = getConfig();
+        console.log(configurations)
+        if (configurations.rootDir === vscode.workspace.rootPath){
+            
+            let terminalObj = vscode.window.createTerminal(`application`);
+            terminalObj.sendText(`npm run dev`);
+            terminalObj.show();
+            vscode.window.createTerminal(`transpile`).sendText(`npm run babel-dev`);
+        } else {
+            vscode.window.showInformationMessage(`we're not in the right folder` + configurations.rootDir);
+        }
     });
 
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
