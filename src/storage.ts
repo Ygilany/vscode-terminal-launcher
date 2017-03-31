501792ad5cd2b442a4b6b84c49f9eccb40a921a8
Yahya Gilany <yahya.gilany@live.com>
2017-03-31 18:37:37 -0400

    addToProjectList function takes in a command instead of command name and script

--- src/storage.ts
+++ src/storage.ts
@@ -39,11 +39,7 @@ export class ProjectStorage {
     this.projectList = [] as ProjectList;
   }
 
-  public addToProjectList(project_name: string, root_path: string, command_name: string, command_script: string): void {    
-    const command: TerminalCommand = {
-      name: command_name,
-      script: command_script
-    }
+  public addToProjectList(project_name: string, root_path: string, command: TerminalCommand): void {    
     this.projectList.push(new ProjectItem(project_name, root_path, command));
     return;
   }
