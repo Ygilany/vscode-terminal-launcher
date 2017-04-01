a5821f4000c473d919330764738b5c6cc4747861
Yahya Gilany <yahya.gilany@live.com>
2017-03-31 23:53:55 -0400

    lintinf

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,9 @@ import * as _ from 'lodash';
 import { Project } from './storage';
 
 export class Utils {
-  public getIndexWherePropertyIs<T>(array:T[], property_name: string, value: string): number {
+  public getIndexWherePropertyIs<T>(array: T[], property_name: string, value: string): number {
     return _.findIndex(array, (o) => {
       return o[property_name] === value;
-    });;
+    });
   }
 }
